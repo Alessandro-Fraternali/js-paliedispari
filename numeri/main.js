d73626bb447a32bbf4cd5b53540a3f19fe888707
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-30 19:36:09 +0200

    Ciclo per la scelta del numero e funzione per randomizzare completati

--- numeri/main.js
+++ numeri/main.js
@@ -7,13 +7,23 @@ var numPC2;
 var min = 1;
 var max = 5;
 
-// alert per il gioco
-alert("PC1 punterà sempre pari, PC2 punterà sempre dispari");
-
 // ciclo for per simulare PC1 e PC2 che scelgono i loro numeri
-// numero di PC1
-// numero di PC2
+for (var i = 0; i < 2; i++) {
+  // numero di PC1
+  if (i==0) {
+    numPC1 = randomizzatore(min, max);
+    console.log(numPC1);
+  }
+  // numero di PC2
+  else {
+    numPC2 = randomizzatore(min, max);
+    console.log(numPC2);
+  }
+}
 
 // funzione per generare un numero da 1 a 5
+function randomizzatore(min, max){
+  return randomizzato = Math.floor(Math.random() * (max - min +1) + min);
+}
 
 // dichiarazione del vincitore e somma dei due numeri
