f5c5a2553f2a10cda81cd0fc999e073365517d16
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-30 18:30:15 +0200

    Inseriti commenti guida

--- numeri/main.js
+++ numeri/main.js
@@ -3,4 +3,17 @@
 var numPC1;
 var numPC2;
 
-// generazione di numeri casuali da 1 a 5
+// variabili per il randomizzatore
+var min = 1;
+var max = 5;
+
+// alert per il gioco
+alert("PC1 punterà sempre pari, PC2 punterà sempre dispari");
+
+// ciclo for per simulare PC1 e PC2 che scelgono i loro numeri
+// numero di PC1
+// numero di PC2
+
+// funzione per generare un numero da 1 a 5
+
+// dichiarazione del vincitore e somma dei due numeri
